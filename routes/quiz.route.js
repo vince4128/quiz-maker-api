379bb004084f7507903d4b9dca16760d78d080ec
vince4128 <vrlt.vincent@gmail.com>
2018-09-25 17:50:07 +0200

    post question route added

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -7,7 +7,7 @@ const   express = require("express"),
 // Index show all quiz
 router.get("/", (req,res)=>{
     //get all otem from db
-    Quiz.find({}, (err, allQuiz)=>{
+    Quiz.find({}).populate("author","-password -created -__v").exec((err, allQuiz)=>{
         if(err){
             console.log(err)
         }else{
@@ -24,14 +24,14 @@ router.post("/", requireAuth, (req,res)=>{
     }
     
     const title = req.body.title;
-    //const author = req.body.author;
-    //const category = req.body.category;
+    const author = req.body.author;
+    const category = req.body.category;
     const description = req.body.description;
     const introduction = req.body.introduction;
     const question = req.body.question;
     const image = req.body.image;
 
-    const newQuiz = {title,description,introduction,question,image};
+    const newQuiz = {title,author,category,description,introduction,question,image};
 
     Quiz.create(newQuiz, (err,newlyCreated)=>{
         if(err){
@@ -44,21 +44,52 @@ router.post("/", requireAuth, (req,res)=>{
 
 });
 
+// Add a question
+router.post("/:id/question", requireAuth, (req,res) => {
+    //find the quiz
+    Quiz.findById(req.params.id, (err, foundQuiz) => {
+        if(err){
+            console.log(err);            
+        }else{
+
+            const statement = req.body.statement;
+            const feedback = req.body.feedback;
+            const type = req.body.type;
+            const proposal = req.body.proposal;
+
+            const newQuestion = {statement, feedback, type, proposal};
+
+            foundQuiz.question.push(newQuestion);
+
+            foundQuiz.save((err, q)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(q);
+                }
+            })
+
+            res.send(newQuestion);
+
+        }
+    })
+})
+
 // show a quiz
 router.get("/:id", (req,res)=>{
     //find the item with the provided ID
-    Quiz.findById(req.params.id, (err, foundQuiz)=>{
+    Quiz.findById(req.params.id).populate("author","-password -created -__v").exec((err, foundQuiz)=>{
         if(err){
             console.log(err);
         }else{
             console.log(foundQuiz);
             res.send(foundQuiz);
         }
-    });
+    })
 })
 
 // update a quiz
-router.put("/:id", requireAuth, (req,res)=>{    
+router.put("/:id", (req,res)=>{    
     Quiz.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedQuiz)=>{
         if(err){
             console.log(err);
